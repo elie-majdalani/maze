b26e04ef818d2a83838876e6d341bcf4d869def3
“Elie <elie.maj@hotmail.com>
2022-05-23 12:42:06 +0300

    fix(maze)edit in the js lose criteria

--- game.js
+++ game.js
@@ -7,15 +7,19 @@ let gameStarted = true;
 let game = document.querySelector("#game");
 let score = 0;
 
-function winAndGameStated(){
-  return win==true && gameStarted == true
+function winAndGameStated() {
+  debugger
+  return win == true && gameStarted == true
 }
 
 // function resetGame(){
 //   gameStarted= false
 //   win = false
 // }
-
+function resetGame() {
+  let win = false;
+  let gameStarted = true;
+}
 function reset() {
   score = 0
   getScore(score)
@@ -27,9 +31,11 @@ function getScore(scores) {
   document.getElementsByTagName("p")[1].innerHTML = `Your score is ${scores} <button id="reset" onClick="reset()">Reset</button>`
 }
 function gameStart() {
-  if (gameStarted) {
+  console.log(win)
+  console.log(gameStarted)
   game.addEventListener("mouseleave", function () {
-      win = false;
+    win = false;
+    if (gameStarted) {
       for (const element of boundaries) {
         element.classList.add("youlose")
       }
@@ -39,41 +45,52 @@ function gameStart() {
       score -= 10
       getScore(score)
       document.getElementById("status").innerHTML = "YOU LOSE!"
+      // resetGame()
       alert("YOU LOSE! START OVER!");
-    })
+    }
+
+  })
   boundaries.forEach(element => {
     element.addEventListener("mouseover", function () {
+      if (gameStarted) {
         win = false;
         for (const element of boundaries) {
           element.classList.add("youlose")
         }
         score -= 10
         getScore(score)
+        // resetGame()
         document.getElementById("status").innerHTML = "YOU LOSE!"
         alert("YOU LOSE! START OVER!");
       }
+
+    }
     )
   });
   end.addEventListener("mouseover", function () {
-    if (winAndGameStated) {
+    console.log(win)
+    if (gameStarted) {
+      // resetGame()
       score += 5
       getScore(score)
       document.getElementById("status").innerHTML = "YOU WIN!";
       alert("CONGRATULATIONS! YOU WIN!");
     }
-  })}
+  })
 }
+
 window.onload = function () {
   document.getElementById("status").innerHTML = "<br>"
   start.addEventListener("click", function (e) {
-    win = true
-    if (gameStarted) {
-      gameStart()
-    }
+    gameStarted=true
+    gameStart()
+    console.log("game started")
+
     boundaries.forEach(element => {
       element.classList.remove("youlose");
     });
     document.getElementById("status").innerHTML = "Move your mouse over the S to begin";
   })
+
 }
 
