7aad3dbee063d1e8f9a2b8f203b7dbcca5fccc98
“Elie <elie.maj@hotmail.com>
2022-05-24 13:46:46 +0300

    feat(maze)add timer for the score

--- game.js
+++ game.js
@@ -31,6 +31,7 @@ function getScore(scores) {
   document.getElementsByTagName("p")[1].innerHTML = `Your score is ${scores} <button id="reset" onClick="reset()">Reset</button>`
 }
 function gameStart() {
+  startTimer()
   console.log(win)
   console.log(gameStarted)
   game.addEventListener("mouseleave", function () {
@@ -46,6 +47,7 @@ function gameStart() {
       getScore(score)
       document.getElementById("status").innerHTML = "YOU LOSE!"
       // resetGame()
+      resetTimer()
       alert("YOU LOSE! START OVER!");
     }
 
@@ -60,6 +62,7 @@ function gameStart() {
         score -= 10
         getScore(score)
         // resetGame()
+        resetTimer()
         document.getElementById("status").innerHTML = "YOU LOSE!"
         alert("YOU LOSE! START OVER!");
       }
@@ -73,6 +76,17 @@ function gameStart() {
       // resetGame()
       score += 5
       getScore(score)
+      const last = document.getElementById('last');
+      const best = document.getElementById('best');
+      [lmsec, lsec] = stopTimer()
+      bestTime = [bmsec, bsec][0] + [bmsec, bsec][1] * 1000
+      lastTime = ([lmsec, lsec][0] + [lmsec, lsec][1] * 1000)
+      if (((lastTime < bestTime) || ([bmsec, bsec][0]) == 0 && [bmsec, bsec][1] == 0)) {
+        [bmsec, bsec] = stopTimer()
+        best.innerHTML = ` ${lsec} : ${lmsec}`;
+      }
+      last.innerHTML = ` ${lsec} : ${lmsec}`;
+      resetTimer()
       document.getElementById("status").innerHTML = "YOU WIN!";
       alert("CONGRATULATIONS! YOU WIN!");
     }
@@ -82,7 +96,7 @@ function gameStart() {
 window.onload = function () {
   document.getElementById("status").innerHTML = "<br>"
   start.addEventListener("click", function (e) {
-    gameStarted=true
+    gameStarted = true
     gameStart()
     console.log("game started")
 
@@ -94,3 +108,44 @@ window.onload = function () {
 
 }
 
+const timer = document.getElementById('live');
+var [milliseconds, seconds] = [0, 0];
+var int = null;
+var [bmsec, bsec] = [0, 0];
+var [lmsec, lsec] = [0, 0];
+
+
+function displayTimer() {
+  milliseconds += 10;
+
+  if (milliseconds == 1000) {
+    milliseconds = 0;
+    seconds++;
+  }
+  if (seconds < 10) {
+    "0" + seconds
+  }
+  else (seconds)
+  var ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
+  timer.innerHTML = ` ${seconds} : ${milliseconds}`;
+}
+
+function startTimer() {
+  if (int !== null) {
+    clearInterval(int);
+  }
+  int = setInterval(displayTimer, 10);
+}
+
+function stopTimer() {
+  [mil, sec] = [milliseconds, seconds]
+  clearInterval(int);
+  timer.innerHTML = ` ${sec} : ${mil}`;
+  return [mil, sec]
+}
+
+function resetTimer() {
+  clearInterval(int);
+  [milliseconds, seconds] = [0, 0];
+  timer.innerHTML = '00 : 000 ';
+}
